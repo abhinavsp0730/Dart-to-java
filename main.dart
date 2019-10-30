ce2baac47fd1e4465daba03f3bcf3d3a27622e22
abhinavsp0730 <43638955+abhinavsp0730@users.noreply.github.com>
2019-10-31 00:48:48 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,27 @@
+ // The fuction which will upload the image as a file
+  void upload(File imageFile) async {
+    var stream =
+    new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+    var length = await imageFile.length();
+
+    String base =
+        "https://your-render-app.onrender.com";
+
+    var uri = Uri.parse(base + '/analyze');
+
+    var request = new http.MultipartRequest("POST", uri);
+    var multipartFile = new http.MultipartFile('file', stream, length,
+        filename: basename(imageFile.path));
+    //contentType: new MediaType('image', 'png'));
+
+    request.files.add(multipartFile);
+    var response = await request.send();
+    print(response.statusCode);
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+      int l = value.length;
+      txt = value;
+
+      setState(() {});
+    });
+  }
